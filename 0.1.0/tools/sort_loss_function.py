7f4708a25470a139466cc316ab97c080664c9916
SkyWorld117 <46778564+SkyWorld117@users.noreply.github.com>
2021-01-28 16:13:01 +0800

    Delete 0.1.0 directory

--- 0.1.0/tools/sort_loss_function.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def Loss_Function(Q, start_position, batch_size, output_matrix, sample_matrix):
-    loss = 0
-    for i in range(start_position, start_position+batch_size):
-        if np.argmax(output_matrix[i]) != np.argmax(sample_matrix[i]):
-            loss += 1
-    Q.put(loss)
