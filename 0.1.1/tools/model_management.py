8b304b5122c4d6681d6e1fe50e62b01476684e85
SkyWorld117 <46778564+SkyWorld117@users.noreply.github.com>
2021-01-28 16:13:26 +0800

    Delete 0.1.1 directory

--- 0.1.1/tools/model_management.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def save_model(variable, path):
-    with open(path, 'wb') as f:
-        pickle.dump(variable, f)
-
-def load_model(path):
-    with open(path, 'rb') as f:
-        return pickle.load(f)
