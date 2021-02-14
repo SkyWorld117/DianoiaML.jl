3e011fe73fed670d5ddc17e51a674bd4d83ec4fc
SkyWorld117 <46778564+SkyWorld117@users.noreply.github.com>
2021-02-15 00:23:08 +0800

    Add files via upload

--- src/tools/one_hot.jl
+++ src/tools/one_hot.jl
@@ -1,9 +1,9 @@
 using .Threads
 
-function One_Hot(index::Array{Int64}, depth::Int64, dict::Dict; on_value::Float32=1.0f0, off_value::Float32=0.0f0)
-    output_data = fill(off_value, (depth, size(index, 1)))
-    @threads for i in 1:length(index)
-        output_data[dict[index[i]],i] = on_value
+function One_Hot(input::Array{Int64}, depth::Int64, dict::Dict; on_value::Float32=1.0f0, off_value::Float32=0.0f0)
+    output_data = fill(off_value, (depth, size(input, 1)))
+    @threads for i in 1:length(input)
+        output_data[dict[input[i]],i] = on_value
     end
     return output_data
 end
