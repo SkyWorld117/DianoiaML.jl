3afc1b04e25063aba94f27d035ebc6b84d88e312
SkyWorld117 <46778564+SkyWorld117@users.noreply.github.com>
2021-02-15 00:16:21 +0800

    Add files via upload

--- src/tools/flatten.jl
+++ src/tools/flatten.jl
@@ -1,20 +1,19 @@
 using .Threads
 
-# DataSet (Batch_Size, ?, ?...)
-function flatten(DataSet::Any, batch_dim::Int64)
-    dims = ndims(DataSet)
+function flatten(Dataset::Any, batch_dim::Int64)
+    dims = ndims(Dataset)
     len = 1
     for i in 1:dims
         if i!=batch_dim
-            len *= size(DataSet, i)
+            len *= size(Dataset, i)
         end
     end
-    input_data = zeros(Float32, (len, size(DataSet, batch_dim)))
-    @threads for i in axes(DataSet, batch_dim)
+    input_data = zeros(Float32, (len, size(Dataset, batch_dim)))
+    @threads for i in axes(Dataset, batch_dim)
         if dims==3
-            input_data[:,i] = Array{Float32}(reshape(transpose(selectdim(DataSet, batch_dim, i)), (len,1)))
+            input_data[:,i] = Array{Float32}(reshape(transpose(selectdim(Dataset, batch_dim, i)), (len,1)))
         else
-            input_data[:,i] = Array{Float32}(reshape(selectdim(DataSet, batch_dim, i), (len,1)))
+            input_data[:,i] = Array{Float32}(reshape(selectdim(Dataset, batch_dim, i), (len,1)))
         end
     end
     return input_data
