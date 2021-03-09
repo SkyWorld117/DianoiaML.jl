812cfaf37b8635b043fd1d4b78b91512fdadb76e
Yi Zhu <46778564+SkyWorld117@users.noreply.github.com>
2021-03-09 22:11:10 +0800

    Delete src/tools directory

--- src/tools/glorotuniform.jl
+++
@@ -1,8 +0,0 @@
-module GlorotUniform
-    function GUMatrix(input_size::Int64, layer_size::Int64, shape::Any)
-        return rand(-sqrt(6/(input_size+layer_size)):0.00001:sqrt(6/(input_size+layer_size)), shape)
-    end
-    export GUMatrix
-end
-
-# Source: https://keras.io/api/layers/initializers/#glorotuniform-class
